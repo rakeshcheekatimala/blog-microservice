4e460fa370ed22e757d8fd1b8db9027df245a195
rakeshcheekatimala <rakeshcheekatimala@gmail.com>
2020-06-22 04:39:45 +0800

    initial commit for posts & comments implementation of get,post methods

--- comments/index.js
+++ comments/index.js
@@ -1,10 +1,26 @@
 import express from 'express';
-const app = express();
+import bodyparser from 'body-parser'
+import {randomBytes} from 'crypto';
 
-app.get('/', function (req, res) {
-  res.send('Hello World! from comments App');
+const app = express();
+app.use(bodyparser.json());
+let commentsByPostId = {};
+
+app.get('/posts/:id/comments',  (req, res)=> {
+  res.send(commentsByPostId[req.params.id]||[]);
 });
 
-app.listen(3001, function () {
-  console.log('Example Comments listening on port 3000!');
+app.post('/posts/:id/comments',  (req, res) => {
+  let postId = req.params.id; // postid
+  let comments = commentsByPostId[postId] || [];
+  let commentId = randomBytes(4).toString('hex');
+  let {content} = req.body;
+  comments.push({id:commentId,content});
+  commentsByPostId[postId] = comments;
+  res.status(201).send(comments);
+});
+
+
+app.listen(4001, function () {
+  console.log('Example Comments listening on port 4001!');
 });
--- posts/index.js
+++ posts/index.js
@@ -1,10 +1,26 @@
 import express from 'express';
+import bodyparser from 'body-parser';
+import {randomBytes} from 'crypto';
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(bodyparser.json());
+
+let posts = {};
+
+app.get('/posts',  (req, res)=> {
+  res.send(posts);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.post('/posts',(req,res)=>{
+  let title = req.body.title;
+  let id = randomBytes(4).toString('hex'); // generate randombytes and convert to hex value
+  
+  posts[id] = {
+    id,
+    title
+  }
+  res.status(201).json(posts);
+})
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!');
 });
